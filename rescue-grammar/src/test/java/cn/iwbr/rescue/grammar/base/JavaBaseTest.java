e824552155218c2e6ed66bbe07e8f5f5ffc238c5
ricardowan <ricardomrwang@gmail.com>
2024-03-06 01:17:12 +0800

    add: 提交一些测试代码

--- rescue-grammar/src/test/java/cn/iwbr/rescue/grammar/base/JavaBaseTest.java
+++ rescue-grammar/src/test/java/cn/iwbr/rescue/grammar/base/JavaBaseTest.java
@@ -3,15 +3,14 @@ package cn.iwbr.rescue.grammar.base;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootTest
 public class JavaBaseTest {
 
     @Test
-    public void ExplicitTypeCasting(){
+    public void ExplicitTypeCasting() {
         short i = 1;
         i = (short) (i + 1);
         i += 1;
@@ -24,8 +23,8 @@ public class JavaBaseTest {
         int age = 10;
 
         Map map1 = new HashMap<>();
-        map1.put("name",person);
-        map1.put("age",age);
+        map1.put("name", person);
+        map1.put("age", age);
 
         Map map2 = new HashMap<>();
         map2.putAll(map1);
@@ -38,6 +37,82 @@ public class JavaBaseTest {
         System.out.println(map1.get("age"));
         System.out.println(map2.get("age"));
 
+    }
+
+    @Test
+    public void test01() {
+//        String str1 = new StringBuilder("jaa").append("va").toString();
+//        String str2 = str1.intern();
+//        System.out.println(str1 == str2);
+//
+//        String str3 = new StringBuilder("hello").append("world").toString();
+//        String str4 = str3.intern();
+//        System.out.println(str3 == str4);
+
+        String str6 = "hello world";
+        String str5 = "hello world";
+
+        System.out.println(str5 == str6);
+
+        String str7 = new String("hello world");
+        String str8 = new String("hello world");
+
+        System.out.println(str7 == str8);
+
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "ab";
+        String s4 = "a" + "b";
+        String s5 = s1 + s2;
+        String s6 = s1 + "b";
+        System.out.println(s3 == s4);
+        System.out.println(s3 == s5);
+        System.out.println(s5 == s6);
+    }
+
+    @Test
+    public void ListTest(){
+        List list = new ArrayList();
+        List test = new LinkedList();
+        test.add("adsad");
+
+        Map<String, String> map = new HashMap();
+        map.put("name","12jhasd");
+        map.put("age","adadsad");
+        map.put("gender","8723jhs");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+        System.out.println();
+
+        Map<String, String> map1 = new LinkedHashMap();
+        map1.put("name","12jhasd");
+        map1.put("age","adadsad");
+        map1.put("gender","8723jhs");
+
+        for (Map.Entry<String, String> entry : map1.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+        System.out.println();
+
+        Map<String, String> map2 = new ConcurrentHashMap<>();
+        map2.put("name","12jhasd");
+        map2.put("age","adadsad");
+        map2.put("gender","8723jhs");
+        map2.put("genderdd",null);
+
+        // ConcurrentHashMap 不建议使用这种遍历方式
+        for (Map.Entry<String, String> entry : map2.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+
+        // ConcurrentHashMap 使用弱一致性的遍历方法：迭代器可能不会反映最新的修改，而是在某一时刻之前的状态
+        Iterator<Map.Entry<String, String>> iterator = map2.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> next = iterator.next();
+            System.out.println(next.getKey() + "=" + next.getValue());
+        }
     }
 
     public class Person extends AbstractClassTest{
